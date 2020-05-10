ae37867eda29d94b7c3223c277cd3e3c1da390a0
jianzhong.rong <tony.tiger.rong@gmail.com>
2020-05-10 09:51:28 -0300

    14 binding event: using arrow function for event handler function, auto bind this keyword to the handler function

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -27,13 +27,41 @@ export class Counter extends Component {
             }
         );
     }
+    testThis1(){
+        console.log(this);
+    }
+    testThis2(){
+        console.log(this);
+        this.setState(
+            preState=>({
+                count: preState.count - 1
+            }),
+            ()=>{
+                console.log("Call back value", this.state.count);
+            }
+        );
+    }
+    testThis3 = ()=>{
+        this.setState(
+            preState => (
+                {
+                    count: preState.count + 2
+                }
+            )
+        );
+    }
     render() {
         return (
             <div>
                Counter: {this.state.count}
                <button onClick={()=>this.increment()}>increment</button>
-               
+               {/* arrow function will bind this keyword to function automatically */}
                <button onClick={()=>this.decrement()}>decrement</button>
+               <button onClick={this.testThis1}>TestThisKeyword1</button>
+               {/* have to use bind(this) to bind this keyword to the event hanlder function */}
+               <button onClick={this.testThis2.bind(this)}>TestThisKeyword2</button>
+               {/* define the event handler function by arrow function can bind this automatically */}
+               <button onClick={this.testThis3}>TestThis3</button>
             </div>
         )
     }
